c32df1e124ecf73d5657bfd59fde20310d0e84f3
Tarun Reddy <tarun.reddy@penthara.com>
2023-01-28 17:52:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section id="home">
+      <h1>Welcome to My Website</h1>
+      <p>This is a sample website.</p>
+    </section>
+    <section id="about">
+      <h1>About Us</h1>
+      <p>Learn more about our company and team.</p>
+    </section>
+    <section id="contact">
+      <h1>Contact Us</h1>
+      <p>Get in touch with us.</p>
+    </section>
     </div>
   );
 }
